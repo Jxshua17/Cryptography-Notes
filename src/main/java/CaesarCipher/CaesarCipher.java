e2b494e5f0afd8feb72523727c8f903033723823
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-03-03 13:54:43 +0100

    Implemented the Caesar Cipher(decryption).
    created a new method decryption which is the reverse of the encryption method. this takes in the ciphertext and the key and returns the plaintext.

--- src/main/java/CaesarCipher/CaesarCipher.java
+++ src/main/java/CaesarCipher/CaesarCipher.java
@@ -60,4 +60,71 @@ public class CaesarCipher {
 
         return cipherText.toString();
     }
+
+    public static String decryption(String ciphertext, int key){
+        StringBuilder plaintext = new StringBuilder();
+
+        HashMap<String, Integer> alphabets = new HashMap<>();
+        alphabets.put("a", 0);
+        alphabets.put("b", 1);
+        alphabets.put("c", 2);
+        alphabets.put("d", 3);
+        alphabets.put("e", 4);
+        alphabets.put("f", 5);
+        alphabets.put("g", 6);
+        alphabets.put("h", 7);
+        alphabets.put("i", 8);
+        alphabets.put("j", 9);
+        alphabets.put("k", 10);
+        alphabets.put("l", 11);
+        alphabets.put("m", 12);
+        alphabets.put("n", 13);
+        alphabets.put("o", 14);
+        alphabets.put("p", 15);
+        alphabets.put("q", 16);
+        alphabets.put("r", 17);
+        alphabets.put("s", 18);
+        alphabets.put("t", 19);
+        alphabets.put("u", 20);
+        alphabets.put("v", 21);
+        alphabets.put("w", 22);
+        alphabets.put("x", 23);
+        alphabets.put("y", 24);
+        alphabets.put("z", 25);
+
+
+        char [] alphabets1 = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        int lengthOfCiphertext = ciphertext.length();
+
+        for (int i=0;i<lengthOfCiphertext;i++){
+            if (ciphertext.charAt(i) == ' '){
+                plaintext.append(' ');
+            } else {
+                String a = String.valueOf(ciphertext.charAt(i));
+                int key1 = alphabets.get(a);
+                int newKey = key1 - key;
+
+                if (newKey<0){
+                    newKey = newKey + 26;
+                }
+
+                newKey = newKey%26;
+                char character = alphabets1[newKey];
+
+                plaintext.append(character);
+            }
+        }
+
+        return plaintext.toString();
+    }
+
+    public static void main(String[] args) {
+        String o = decryption("ifmmp", 1);
+        String o1 = decryption("rkbbc", 2);
+        System.out.println(o);
+        System.out.println(o1);
+        System.out.println(encryption("pizza", 2));
+    }
 }
